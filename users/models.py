74101b1735de17b8c3577d947f664d0ac532d5cc
DanuloM <danulo1337@gmail.com>
2026-03-20 17:41:16 +0200

    Add custom User model with phone, location and verification fields

--- users/models.py
+++ users/models.py
@@ -4,4 +4,9 @@ from django.db import models
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"

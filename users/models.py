e5b661e158eb49b2bbd0350ee9adaf59813e62a3
DanuloM <danulo1337@gmail.com>
2026-03-20 18:06:47 +0200

    Add user roles and IsAdmin permission

--- users/models.py
+++ users/models.py
@@ -2,11 +2,16 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class UserRole(models.TextChoices):
+    USER = 'user', 'User'
+    ADMIN = 'admin', 'Admin'
+
 
 class User(AbstractUser):
     phone = models.CharField(max_length=20, null=True, blank=True)
     location = models.CharField(max_length=100, null=True, blank=True)
     is_verified = models.BooleanField(default=False)
+    role = models.CharField(max_length=10, choices=UserRole.choices, default=UserRole.USER)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
